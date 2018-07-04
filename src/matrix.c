d61357cd7267bb2007f1b2c78171ed6fbfe522fe
Flavien <fkoehler@student.42.fr>
2018-07-04 13:30:50 +0200

    refacto mvp calculation

--- src/matrix.c
+++ src/matrix.c
@@ -6,7 +6,7 @@
 /*   By: fkoehler <fkoehler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/06 12:25:21 by fkoehler          #+#    #+#             */
-/*   Updated: 2018/07/04 12:53:44 by fkoehler         ###   ########.fr       */
+/*   Updated: 2018/07/04 13:29:58 by fkoehler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void	mvp_update(t_matrices *matrices)
 {
-	matrices->mvp = mat4_mul(matrices->view, matrices->proj);
-	matrices->model = mat4_mul(matrices->translate, matrices->rotate);
-	matrices->model = mat4_mul(matrices->model, matrices->scale);
-	matrices->mvp = mat4_mul(matrices->model, matrices->mvp);
+	matrices->model = mat4_mul(matrices->scale,
+	mat4_mul(matrices->rotate, matrices->translate));
+	matrices->mvp = mat4_mul(matrices->model,
+	mat4_mul(matrices->view, matrices->proj));
 }
