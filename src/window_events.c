e2a0e6a686e804e55310868f3a02210c1b39dd87
Flavien <fkoehler@student.42.fr>
2018-07-24 14:29:51 +0200

    add model position reset button + refacto win events

--- src/window_events.c
+++ src/window_events.c
@@ -6,7 +6,7 @@
 /*   By: fkoehler <fkoehler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/04 17:18:23 by fkoehler          #+#    #+#             */
-/*   Updated: 2018/07/24 14:04:06 by fkoehler         ###   ########.fr       */
+/*   Updated: 2018/07/24 14:28:32 by fkoehler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,21 +48,11 @@ static void	rotate(t_matrices *matrices, int key)
 		matrices->rotate = mat4_rotate(matrices->rotate, 0.04f, Z_AXIS);
 	if (key == GLFW_KEY_PAGE_DOWN)
 		matrices->rotate = mat4_rotate(matrices->rotate, -0.04f, Z_AXIS);
+	mvp_update(matrices);
 }
 
-void		key_callback(GLFWwindow* win, int key, int scanc, int action, int mods)
+static void	translate(t_matrices *mat, int key)
 {
-	t_matrices	*mat;
-
-	(void)scanc;
-	(void)mods;
-	mat = get_matrices();
-	if (action == GLFW_RELEASE)
-		return ;
-	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-		glfwSetWindowShouldClose(win, 1);
-	if (key >= GLFW_KEY_RIGHT && key <= GLFW_KEY_PAGE_DOWN)
-		rotate(get_matrices(), key);
 	if (key == GLFW_KEY_A)
 		mat->translate = mat4_translate(mat->translate, new_vec3(-0.1f, 0, 0));
 	if (key == GLFW_KEY_D)
@@ -76,6 +66,33 @@ void		key_callback(GLFWwindow* win, int key, int scanc, int action, int mods)
 	if (key == GLFW_KEY_E)
 		mat->translate = mat4_translate(mat->translate, new_vec3(0, 0, -0.1f));
 	mvp_update(mat);
+}
+
+static void	reset_position(t_env *env)
+{
+	env->matrices->translate = mat4_translate(new_mat4(MAT_IDENTITY),
+	new_vec3(0, 0, -(env->model->max_coord_interval)));
+	env->matrices->rotate = new_mat4(MAT_IDENTITY);
+	mvp_update(env->matrices);
+}
+
+void		key_callback(GLFWwindow* win, int key, int scanc, int action, int mods)
+{
+	(void)scanc;
+	(void)mods;
+	if (action == GLFW_RELEASE)
+		return ;
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(win, 1);
+		return ;
+	}
+	if (key >= GLFW_KEY_RIGHT && key <= GLFW_KEY_PAGE_DOWN)
+		rotate(get_matrices(), key);
+	if (key >= GLFW_KEY_A && key <= GLFW_KEY_W)
+		translate(get_matrices(), key);
 	if (key >= GLFW_KEY_1 && key <= GLFW_KEY_6)
 		switch_option_state(get_env_struct(NULL), key);
+	if (key == GLFW_KEY_SPACE && action == GLFW_PRESS)
+		reset_position(get_env_struct(NULL));
 }
